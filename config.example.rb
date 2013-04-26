559e0c3784917fc5b4f99808723182d80c1f961c
Ellyll <cawsllyffant-git@yahoo.com>
2013-04-26 21:21:47 +0100

    Added test username/password to config example

--- config.example.rb
+++ config.example.rb
@@ -2,5 +2,6 @@ module Config
   APP = { :name => 'CryoTracker', :default_page_size => 20 }
   DB = { :connection => 'mysql://user:password@localhost/databasename' }
   AUTHENTICATION = { :user_files_directory => '/some/path/vardata/users' }
+  TEST = { :test_user_username => 'testuser', :test_user_password => 'testpassword' }
 end
 
